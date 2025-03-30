7448d392f1accf828f883ac857c7f88e2f8ab676
Egmarortiz <egmarocasio9@gmail.com>
2025-03-30 14:08:27 -0400

    fixing % case in printf

--- _printf.c
+++ _printf.c
@@ -9,41 +9,44 @@
  */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int count = 0, i = 0;
+	va_list args;
+	int count = 0, i = 0;
 
-    if (!format)
-        return (-1);
-    va_start(args, format);
-    while (format[i])
-    {
-        if (format[i] == '%')
-        {
-            i++;
-            if (!format[i])
-                break;
-            if (format[i] == 'c')
-                count += print_char(args);
-            else if (format[i] == 's')
-                count += print_string(args);
-            else if (format[i] == '%')
-                count += print_percent();
-            else
-            {
-                _putchar('%');
-                _putchar(format[i]);
-                count += 2;
-            }
-        }
-        else
-        {
-            _putchar(format[i]);
-            count++;
-        }
-        i++;
-    }
-    va_end(args);
-    return (count);
+	if (!format)
+		return (-1);
+	va_start(args, format);
+	while (format[i])
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			if (!format[i])
+			{
+				va_end(args);
+				return (-1);
+			}
+			if (format[i] == 'c')
+				count += print_char(args);
+			else if (format[i] == 's')
+				count += print_string(args);
+			else if (format[i] == '%')
+				count += print_percent();
+			else
+			{
+				_putchar('%');
+				_putchar(format[i]);
+				count += 2;
+			}
+		}
+		else
+		{
+			_putchar(format[i]);
+			count++;
+		}
+		i++;
+	}
+	va_end(args);
+	return (count);
 }
 
 
