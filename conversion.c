697306f9e243c05fd921a7999aae61999c4d5745
victor10days <victor10days@gmail.com>
2025-03-30 21:12:08 -0400

    second push

--- conversion.c
+++ conversion.c
@@ -1,68 +1,57 @@
 #include "main.h"
-#include <stddef.h>
 #include <stdarg.h>
+#include <unistd.h>
 
-
-int print_char(va_list args)
+/**
+ * _putchar - Writes a character to stdout
+ * @c: character to write
+ * Return: 1 on success, -1 on failure
+ */
+int _putchar(char c)
 {
-	char c = (char)va_arg(args, int);
-
-	return (_putchar(c));
+    return (write(1, &c, 1));
 }
 
-int print_string(va_list args)
-{
-	char *str = va_arg(args, char*);
-	int count = 0;
-	int i;
-
-	if (str == NULL)
-	{
-		str = "(null)";
-		for (i = 0; str[i] != '\0'; i++)
-		{
-			count += _putchar(str[i]);
-		}
-		return (count);
-	}
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		count += _putchar(str[i]);
-	}
-	return (count);
-}
-
-
+/**
+ * print_percent - Prints a percent character
+ * Return: Number of characters printed (always 1)
+ */
 int print_percent(void)
 {
-	return (_putchar('%'));
+    return (_putchar('%'));
 }
 
+/**
+ * print_int - Prints an integer to stdout
+ * @args: variadic list containing integer
+ * Return: Number of characters printed
+ */
 int print_int(va_list args)
 {
     int num = va_arg(args, int);
     unsigned int abs_num;
     int div = 1, len = 0;
 
-if (num < 0)
+    if (num < 0)
     {
         len += _putchar('-');
-        abs_num = -num;         
+        abs_num = -num;
     }
     else
     {
         abs_num = num;
     }
 
-while (abs_num / div > 9)
+    while (abs_num / div > 9)
         div *= 10;
 
-while (div != 0)
+    while (div != 0)
     {
         len += _putchar('0' + (abs_num / div));
         abs_num %= div;
-        div /= 10;                               
+        div /= 10;
     }
-    
+
     return (len);
 }
+
